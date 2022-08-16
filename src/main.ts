8f00d2c08be8757e63d6a76a1362829a1d571825
Mikko Kemppainen <mikko.kemppainen3@gmail.com>
2022-08-17 00:50:42 +0300

    Moving blocks

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { GameLoop, Sprite, init } from 'kontra';
+import { GameLoop, Sprite, init, randInt } from 'kontra';
 
 import { UiElement } from './ui';
 
@@ -12,11 +12,12 @@ const uiElement = new UiElement({
   width: 0.5,
 });
 
-uiElement.render();
+// uiElement.render();
 
 const sprites = Array.from(Array(150).keys()).map((item) => {
   let color = 'white';
-  if (item % 2) color = 'black';
+  if (item % 2) color = 'green';
+  if (item % 3) color = 'red';
   return Sprite({
     x: (item * 16) % 240,
     y: Math.floor(item / 15) * 16,
@@ -24,16 +25,36 @@ const sprites = Array.from(Array(150).keys()).map((item) => {
     width: 16,
     height: 16,
     dx: 0,
+    randirx: 1,//randInt(-5, 5) + 0.5,
+    randiry: 1,//randInt(-5, 5) + 0.5,
   });
 });
 
+let tick = 0;
 const loop = GameLoop({
   update: () => {
     uiElement.update();
-    sprites.forEach((s) => s.update());
-    // if (sprite.x > canvas.width) {
-    //   sprite.x = -sprite.width;
-    // }
+    sprites.forEach((sprite: Sprite) => {
+      sprite.x += sprite.randirx;
+      sprite.y += sprite.randiry;
+      
+      if (!randInt(0, 1000)) {
+        sprite.randirx = randInt(-5,5) + 0.5;
+        sprite.randiry = randInt(-5, 5) + 0.5;
+      }
+      tick++;
+      if (sprite.x > canvas.width) {
+        sprite.x = -sprite.width;
+      }
+
+      if (sprite.y > canvas.height) {
+        sprite.y = -sprite.height;
+      }
+      sprite.update();
+
+    })
+
+
   },
   render: () => {
     sprites.forEach((s) => s.render());
