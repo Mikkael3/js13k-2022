c3783f4ed54465cf10b14fb5f98af4cc6d50e01e
Mikko Kemppainen <mikko.kemppainen@punamusta.com>
2022-08-18 00:54:02 +0300

    Fix monster generator randomness

--- src/monster-generator.ts
+++ src/monster-generator.ts
@@ -1,11 +1,12 @@
+import { randInt } from 'kontra';
 import { classes, races } from './data';
 import { Monster } from './types';
 
 export const generateMonsterSet = (amount = 3): Monster[] => {
   return Array.from(Array(amount)).map((): Monster => {
     return {
-      race: races[Math.floor(Math.random() * (races.length - 1))],
-      class: classes[Math.floor(Math.random() * (classes.length - 1))],
+      race: races[randInt(0, 2)],
+      class: classes[randInt(0, 2)],
       hp: 10,
     };
   });
