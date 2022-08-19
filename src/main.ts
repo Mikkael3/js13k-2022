1b2c9d730e30021454d9a02db08fada8cf149eb2
Mikko Kemppainen <mikko.kemppainen@punamusta.com>
2022-08-19 23:54:20 +0300

    Move monster to own class

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Monster } from './types';
 import { MonsterBox } from './monster-box';
 import { generateMonsterSet } from './monster-generator';
 import { GameUi } from './game-ui';
+import {MonsterC} from "./monster";
 
 const { canvas } = init();
 initPointer();
@@ -36,64 +37,6 @@ const monsterBox = new MonsterBox({
 });
 
 monsterBox.render();
-
-type MonsterProps = Partial<Sprite> & { monster: Monster };
-
-class MonsterC extends SpriteClass {
-  text: Text;
-  monsterData: Monster;
-
-  constructor(props: MonsterProps) {
-    super(props);
-    const { monster } = props;
-    this.monsterData = monster;
-    track(this);
-    this.text = Text({
-      text: `${monster.class.name} ${monster.race.name}`,
-      font: '8px Arial',
-      color: 'red',
-      x: 0,
-      y: 0,
-      width: this.width,
-      anchor: { x: 0.5, y: 0.5 },
-      textAlign: 'center',
-    });
-  }
-
-  /// Mouse events
-  onDown() {
-    // handle on down events on the sprite
-    console.log('click down', this.text.text);
-    this.color = 'blue';
-  }
-
-  onUp() {
-    // handle on up events on the sprite
-    console.log('click up', this.text.text);
-    this.color = 'limegreen';
-  }
-
-  onOver() {
-    console.log('in', this.text.text);
-  }
-
-  onOut() {
-    console.log('out', this.text.text);
-    this.onUp();
-  }
-
-  update(_dt?: number | undefined, time = 0): void {
-    super.update();
-    // Make monster move a little
-    this.x += (Math.sign(Math.cos(time)) * Math.cos(time) ** 2) / 8;
-    this.y += Math.sin(time * 2.1) / 20;
-  }
-
-  draw(): void {
-    super.draw();
-    this.text.draw();
-  }
-}
 
 class Player extends MonsterC {
   update(dt: number, time: number) {
