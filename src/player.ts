e49aae83bfdb9f6e61b583dad04cd215905f2a1f
Mikko Kemppainen <mikko.kemppainen@punamusta.com>
2022-08-27 00:29:34 +0300

    Make player die

--- src/player.ts
+++ src/player.ts
@@ -3,6 +3,9 @@ import { MonsterC } from './monster';
 export class Player extends MonsterC {
   update(dt: number) {
     super.update(dt);
+    if (this.hp <= 0) {
+      this.rotation += 0.1;
+    }
   }
 
   onDown() {
