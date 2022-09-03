f20d366bc06d92eb352878afdb8be05959b927c2
Mikko Kemppainen <mikko.kemppainen@punamusta.com>
2022-09-03 16:32:17 +0300

    Change battle log line style

--- src/battle-log.ts
+++ src/battle-log.ts
@@ -4,7 +4,6 @@ export class BattleLog extends UiElement {
   constructor(props: UiElementProps) {
     super(props);
     const style = this.rootElement.style;
-    style.border = 'darkgrey solid 0.5vh';
     style.display = 'flex';
     style.flexDirection = 'column';
     style.overflow = 'auto';
@@ -14,6 +13,7 @@ export class BattleLog extends UiElement {
   addLine(text: string) {
     const textLine = document.createElement('p');
     textLine.style.margin = '1px';
+    textLine.style.border = '0.25vh dotted black';
     textLine.textContent = text;
     this.rootElement.prepend(textLine);
   }
