d9c3a20be3ae77acb29a09dfd83e5ba0c6a999ee
Mikko Kemppainen <mikko.kemppainen@punamusta.com>
2022-09-09 19:49:16 +0300

    Add a kind of storyboard

---
+++ src/story.ts
@@ -0,0 +1,23 @@
+export const story = [
+  'Just a little girl, I am so helpful an nice.',
+  'Life was is good cause im such a lovely girl.',
+  'But then came the darkness. A goblin attacked us.',
+  'I had a little secret. I could trade my lifo for another.',
+  "But I was too late. I couldn't save no one.",
+  'Then rose the bloodied goblin. He wanted my life.',
+  () => {
+    console.log('intro fight');
+  },
+  "Oh no. I didn't mean to. Even if you killed my parents.",
+  "I don't want to kill you. I will save your life. I will trade in mine.",
+  'But why am I a goblin.',
+  'They left me for dead.',
+  'Oh no. What must we do now.',
+  'I must, take revenge!',
+  "No, I don't want to killl.",
+  'You just have to save them. Just like you saved me.',
+  'Yes, I must save!',
+  () => {
+    console.log('start game');
+  },
+];
