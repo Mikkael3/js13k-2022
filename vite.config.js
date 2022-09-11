aa31c974edf483a9b2ae5b1b48c861283c04835d
Mikko Kemppainen <mikko.kemppainen@punamusta.com>
2022-09-11 21:09:14 +0300

    Use terser vite plugin for higher comperssion

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,12 @@
 export default {
-  
+    build: {
+        minify: 'terser',
+        module: true,
+        terserOptions: {
+            toplevel: true,
+            mangle: {
+                properties: true,
+            }
+        }
+    }
 }
