bc8e1556c5c98be69ee62048d9a7a26e02ff6a9e
Mikko Kemppainen <mikko.kemppainen@punamusta.com>
2022-09-11 21:15:14 +0300

    Revert "Use terser vite plugin for higher comperssion"
    This reverts commit aa31c974edf483a9b2ae5b1b48c861283c04835d.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
 export default {
-    build: {
-        minify: 'terser',
-        module: true,
-        terserOptions: {
-            toplevel: true,
-            mangle: {
-                properties: true,
-            }
-        }
-    }
+  
 }
