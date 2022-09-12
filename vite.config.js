b001d0c97037e07050396bfea8aca248db28d6b0
Mikko Kemppainen <mikko.kemppainen@punamusta.com>
2022-09-12 22:15:56 +0300

    Use terser plugin for compression

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-import { defineConfig } from "vite"
-import { viteSingleFile } from "vite-plugin-singlefile"
+import {defineConfig} from "vite"
+import {viteSingleFile} from "vite-plugin-singlefile"
 
 export default defineConfig({
-  plugins: [viteSingleFile()],
-})
+        plugins: [viteSingleFile()],
+        build: {
+            minify: 'terser',
+            // module: true,
+            terserOptions: {
+                // toplevel: true,
+                // mangle: {
+                //     properties: true,
+                // }
+            }
+        }
+    }
+)
